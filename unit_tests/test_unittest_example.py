fbdc6184f933220331cc49bdc9cfa6aff5b52bd8
Pavel Aliamkin <82835924+pashtetskiii@users.noreply.github.com>
2026-06-29 14:05:40 +0300

    D

--- unit_tests/test_unittest_example.py
+++ unit_tests/test_unittest_example.py
@@ -1,16 +1,45 @@
 import unittest
 import requests
+import sys
 
 
 class TestPostApi(unittest.TestCase):
-    def add(self):
-        return 5 + 5
 
-    def test_get_all_post(self):
-        response = requests.get("https://jsonplaceholder.typicode.com/posts").json()
+    def setUp(self):
+        body = {
+            "title": "fsakjdhfkasjdhflkajsdhlkfjashdfoo",
+            "body": "barasdfaskdjfhlaksdfoiwueysdhgkjashdkfjhalskdjfhasdf",
+            "userId": 1
+        }
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post(
+            'https://jsonplaceholder.typicode.com/posts',
+            json=body,
+            headers=headers
+        )
+        self.post_id = response.json()['id']
+        print(f'Post created: {self.post_id}')
+
+    def tearDown(self) -> None:
+        requests.delete(f'https://jsonplaceholder.typicode.com/posts/{self.post_id}')
+        print(f'Post deleted: {self.post_id}')
+
+    @unittest.skip('Bug - #345345')
+    def test_get_one_post(self):
+        print('test')
+        response = requests.get(f'https://jsonplaceholder.typicode.com/posts/{self.post_id}').json()
+        self.assertEqual(response['id'], self.post_id)
+
+
+class TestIndependent(unittest.TestCase):
+    @unittest.skipIf(sys.platform == 'darwin', 'Not for linux run')
+    def test_get_all_posts(self):
+        print('test')
+        response = requests.get('https://jsonplaceholder.typicode.com/posts').json()
         self.assertEqual(len(response), 100)
 
     def test_add_post(self):
+        print('test')
         body = {
             "title": "fsakjdhfkasjdhflkajsdhlkfjashdfoo",
             "body": "barasdfaskdjfhlaksdfoiwueysdhgkjashdkfjhalskdjfhasdf",
